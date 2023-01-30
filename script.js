f14f28127a2d4585141e7f51ab35740bdcd9b231
lucianoscabora <lucianoscabora@gmail.com>
2023-01-30 00:01:25 -0300

    css adjusts

--- script.js
+++ script.js
@@ -1,64 +1,37 @@
-
 const btn = document.querySelector(".confirmar-btn");
-
-
 const infos = document.querySelectorAll(".info");
-
 const data = document.querySelectorAll(".info");
 
-
-
-infos.forEach(element => {
-    element.addEventListener('keypress', () => {
-        element.classList.add('info-success');
-        removeAlert()
-    })
+infos.forEach((element) => {
+  element.addEventListener("keypress", () => {
+    element.classList.add("info-success");
+    removeAlert();
+  });
 });
 
-
-btn.addEventListener('click', () => {
-    
-    
-    const sds = document.querySelectorAll(".info");
-    sds.forEach((number, index) => {
-            if(!number.value) {
-                number.classList.add('info-danger');
-                displayAlert()
-            }
-    });
-    
-})
-
+btn.addEventListener("click", () => {
+  const sds = document.querySelectorAll(".info");
+  sds.forEach((number, index) => {
+    if (!number.value) {
+      number.classList.add("info-danger");
+      displayAlert();
+    }
+  });
+});
 
 function displayAlert() {
-    const war = document.querySelectorAll(".warning");
+  const war = document.querySelectorAll(".warning");
 
-    war.forEach((number) => {
-        number.classList.add('warning-show')
-    })
+  war.forEach((number) => {
+    number.classList.add("warning-show");
+  });
 }
 
 function removeAlert() {
-    const war = document.querySelectorAll(".warning");
+  const war = document.querySelectorAll(".warning");
 
-    war.forEach((number) => {
-        number.classList.remove('warning-show');
-        number.classList.add('warning')
-    })
+  war.forEach((number) => {
+    number.classList.remove("warning-show");
+    number.classList.add("warning");
+  });
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
