392378d5f6fe3adf281d95363d130972c233e60b
lucianoscabora <lucianoscabora@gmail.com>
2023-02-07 13:40:25 -0300

    more logic adjusts

--- script.js
+++ script.js
@@ -2,11 +2,20 @@ const btnConfirm = document.querySelector(".confirmar-btn");
 const formFields = document.querySelectorAll(".info");
 
 formFields.forEach((item) => {
-  item.addEventListener("keypress", () => {
+  item.addEventListener("input", () => {
     item.nextElementSibling.style.visibility = "hidden"
     item.style.border = "2px solid green"; 
   });
 });
+
+formFields.forEach((item) => {
+  item.addEventListener("input", () => {
+    if(item.value.length === 0) {
+      item.nextElementSibling.style.visibility = "visible"
+      item.style.border = "2px solid red"; 
+  }}
+  )
+})
 
 btnConfirm.addEventListener("click", () => {
   formFields.forEach((item) => {
@@ -16,3 +25,5 @@ btnConfirm.addEventListener("click", () => {
     }
   });
 });
+
+
